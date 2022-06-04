1884cdac0213d976d8b5697c61c768cf151e5460
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-06-04 09:54:46 +0700

    Patch V1.1

--- public/symlink.php
+++ public/symlink.php
@@ -1,5 +1,5 @@
 <?php
-$targetFolder = __DIR__.'/storage/app/public';
+$targetFolder = __DIR__.'/../storage/app/public';
 $linkFolder = __DIR__.'/storage';
 symlink($targetFolder,$linkFolder);
 echo 'Symlink process successfully completed';
