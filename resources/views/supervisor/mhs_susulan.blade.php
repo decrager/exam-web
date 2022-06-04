26d6c91383454610a666921858d9d819becea1f4
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-06-04 15:10:56 +0700

    Patch V1.1.1

--- resources/views/supervisor/mhs_susulan.blade.php
+++ resources/views/supervisor/mhs_susulan.blade.php
@@ -147,7 +147,7 @@
                                             </div>
                                             <div class="form-group">
                                                 <h6>Bukti Persyaratan</h6>
-                                                <a href="{{ Storage::files('storage/files/syarat/'. $mahasiswa?->file) }}" target="_blank" class="btn btn-success btn-sm mt-1"> <i class="fas fa-eye"></i>&ensp; Lihat</a>
+                                                <a href="{{ asset('storage/files/syarat/'. $mahasiswa?->file) }}" target="_blank" class="btn btn-success btn-sm mt-1"> <i class="fas fa-eye"></i>&ensp; Lihat</a>
                                             </div>
                                         </div>
                                     </div>
