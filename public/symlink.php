1dc5948de6e1ca544e17917bbe287719d8ea99ef
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-10-02 11:14:51 +0700

    Patch V3.1.0

--- public/symlink.php
+++ public/symlink.php
@@ -1,6 +1,6 @@
 <?php
-$targetFolder = __DIR__.'/../exam-web/storage/app/public';
-$linkFolder = __DIR__.'/storage';
+$targetFolder = __DIR__.'/exam-web/storage/app/public';
+$linkFolder = __DIR__.'/public/storage';
 symlink($targetFolder,$linkFolder);
 echo 'Symlink process successfully completed';
 ?>
